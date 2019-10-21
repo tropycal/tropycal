434860f8d3dbc78463e02e235d97b8507b3bea3a
tomerburg <tburg@albany.edu>
2019-10-21 17:51:47 -0500

    Updated travis file

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'Tropycal Developers'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------

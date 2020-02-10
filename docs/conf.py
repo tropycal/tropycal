3ad82fa2ac9d6352dc11920fb1969fecd0bd9649
tomerburg <tburg@albany.edu>
2020-02-10 10:56:20 -0600

    Update to version 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Tropycal Developers'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------

a7985b81546c95caeaf12796ba490a6b35ee7a0e
tomerburg <tomer.burg@ou.edu>
2021-08-14 15:35:43 -0500

    fix: update to v0.2.10

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Tropycal Developers'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.9'
+release = '0.2.10'
 
 
 # -- General configuration ---------------------------------------------------

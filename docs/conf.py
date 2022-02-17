e0afd52fb00db8d4562d9c9409792754e0bf9cf8
tomerburg <tomer.burg@ou.edu>
2022-02-17 17:45:32 -0500

    feat: update to v0.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Tropycal Developers'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.3.3'
 
 
 # -- General configuration ---------------------------------------------------

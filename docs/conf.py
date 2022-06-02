cf706d78e20cc75c7f5179f88d497cee6a53b58d
Tomer Burg <tomer.burg@ou.edu>
2022-06-01 21:14:28 -0400

    feat: update to v0.4

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Tropycal Developers'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.3.4'
+release = '0.4'
 
 
 # -- General configuration ---------------------------------------------------

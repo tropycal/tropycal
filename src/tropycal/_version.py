747064730e9cda30e29f09dc014716cc4c77640d
Miro <200482516+Mirochill@users.noreply.github.com>
2026-05-23 19:53:26 +0200

    Replace pkg_resources version fallback

--- src/tropycal/_version.py
+++ src/tropycal/_version.py
@@ -8,5 +8,8 @@ def get_version():
         return get_version(root='..', relative_to=__file__,
                            version_scheme='post-release', local_scheme='dirty-tag')
     except (ImportError, LookupError):
-        from pkg_resources import get_distribution
-        return get_distribution(__package__).version
+        try:
+            from importlib.metadata import version as distribution_version
+        except ImportError:
+            from importlib_metadata import version as distribution_version
+        return distribution_version(__package__)
